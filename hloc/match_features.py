0de9a55a1cbcd9e9d56672b7ece003c511506874
Dávid Komorowicz <dawars00@gmail.com>
2024-04-18 21:38:20 +0200

    Make logged path absolute

--- hloc/match_features.py
+++ hloc/match_features.py
@@ -233,7 +233,7 @@ def match_from_paths(conf: Dict,
     if is_slurm:
         slurm_id = os.environ["SLURM_JOB_ID"]
         pairs_cache_path = pairs_path.with_name(f"{slurm_id}_pairs.txt")
-        logger.info(f"Pairs cache path {pairs_cache_path}")
+        logger.info(f"Pairs cache path {pairs_cache_path.absolute()}")
         # if cache pairs file already exists, load that instead
         if pairs_cache_path.exists():
             logger.info("Pair cache exists")
