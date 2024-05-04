8ea1b333a2e85eb91f07a6f9443214a176e8aeff
hexingyi <13073523573@163.com>
2024-05-04 15:34:34 +0800

    Update

--- hloc/utils/io.py
+++ hloc/utils/io.py
@@ -4,9 +4,6 @@ import numpy as np
 import cv2
 import h5py
 
-from .parsers import names_to_pair, names_to_pair_old
-
-
 def read_image(path, grayscale=False):
     if grayscale:
         mode = cv2.IMREAD_GRAYSCALE
@@ -32,42 +29,20 @@ def list_h5_names(path):
 
 def get_keypoints(path: Path, name: str,
                   return_uncertainty: bool = False) -> np.ndarray:
-    with h5py.File(str(path), 'r', libver='latest') as hfile:
-        dset = hfile[name]['keypoints']
+    with h5py.File(str(path), 'r') as hfile:
+        dset = hfile[name]
         p = dset.__array__()
-        uncertainty = dset.attrs.get('uncertainty')
     if return_uncertainty:
-        return p, uncertainty
+        return p, None
     return p
 
 
 def find_pair(hfile: h5py.File, name0: str, name1: str):
-    pair = names_to_pair(name0, name1)
-    if pair in hfile:
-        return pair, False
-    pair = names_to_pair(name1, name0)
-    if pair in hfile:
-        return pair, True
-    # older, less efficient format
-    pair = names_to_pair_old(name0, name1)
-    if pair in hfile:
-        return pair, False
-    pair = names_to_pair_old(name1, name0)
-    if pair in hfile:
-        return pair, True
-    raise ValueError(
-        f'Could not find pair {(name0, name1)}... '
-        'Maybe you matched with a different list of pairs? ')
-
+    return ' '.join([name0, name1]), False
 
 def get_matches(path: Path, name0: str, name1: str) -> Tuple[np.ndarray]:
     with h5py.File(str(path), 'r', libver='latest') as hfile:
         pair, reverse = find_pair(hfile, name0, name1)
-        matches = hfile[pair]['matches0'].__array__()
-        scores = hfile[pair]['matching_scores0'].__array__()
-    idx = np.where(matches != -1)[0]
-    matches = np.stack([idx, matches[idx]], -1)
-    if reverse:
-        matches = np.flip(matches, -1)
-    scores = scores[idx]
+        matches = hfile[pair].__array__().T
+        scores = np.ones((matches.shape[0],))
     return matches, scores
