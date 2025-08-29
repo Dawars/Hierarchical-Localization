3567d7122b4b59459dc2e0eb5148ae597ebadc76
Dávid Komorowicz <dawars00@gmail.com>
2025-08-29 15:21:45 +0200

    Update extract_features.py
    Only filter keypoint_scores if available (superpoint)

--- hloc/extract_features.py
+++ hloc/extract_features.py
@@ -402,7 +402,8 @@ def main(conf: Dict,
                 valid_keypoint = mask[pred['keypoints'][:, 1].astype('int'), pred['keypoints'][:, 0].astype('int')]
                 pred['keypoints'] = pred['keypoints'][valid_keypoint > 0]
                 pred['descriptors'] = pred['descriptors'][:, valid_keypoint > 0]
-                pred['keypoint_scores'] = pred['keypoint_scores'][valid_keypoint > 0]
+                if "keypoint_scores" in pred:
+                    pred['keypoint_scores'] = pred['keypoint_scores'][valid_keypoint > 0]
 
         if as_half:
             for k in pred:
